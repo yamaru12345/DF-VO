5757d31e120e30938b44ebd6da072ffe2d5f9edf
yamaru12345 <65715903+yamaru12345@users.noreply.github.com>
2021-01-15 21:46:20 +0900

    Update vo_modules.py

--- vo_modules.py
+++ vo_modules.py
@@ -694,9 +694,7 @@ class VisualOdometry():
         flow_net_tracking = self.deep_models['flow'].inference_kp
         batch_size = self.cfg.deep_flow.batch_size
         num_forward = int(np.ceil(len(ref_data['id']) / batch_size))
-        print(np.sort(ref_data['depth'][ref_id].flatten()))
-        depth_range_mask = (ref_data['depth'][ref_id] < self.cfg.depth.min_depth) + (ref_data['depth'][ref_id] > self.cfg.depth.max_depth)
-        mask = depth_range_mask + cur_data['mask'] + ref_data['mask'][ref_data['id'][0]]
+        mask = np.logical_not(ref_data['depth'][ref_data['id'][0]]) + cur_data['mask'] + ref_data['mask'][ref_data['id'][0]]
         for i in range(num_forward):
             # Read precomputed flow / real-time flow
             batch_kp_ref_best, batch_kp_cur_best, batch_kp_ref_regular, batch_kp_cur_regular, batch_flows = flow_net_tracking(
